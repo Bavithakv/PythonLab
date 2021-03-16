9cdd35757b221bca6c1cb581dfa4248713e16385
Bavithakv <79898409+Bavithakv@users.noreply.github.com>
2021-03-16 21:07:31 +0530

    Delete area of square,rect,triangle_11.py

--- CO2/area of square,rect,triangle_11.py
+++
@@ -1,8 +0,0 @@
-square = lambda a: a * 2
-print("Area of the square is", square(10))
-
-rectangle = lambda a, b: a * b
-print("Area of the rectangle is", rectangle(10, 5))
-
-triangle = lambda b, h: (b * h) / 2
-print("Area of the rectangle is", triangle(4, 2))
